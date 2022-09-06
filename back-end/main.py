4559317ce0ae5ef06a0253ebe99dfec24d71b7e7
Claire <clairehh@live.unc.edu>
2022-09-06 18:16:29 -0400

    this is where the top-of-show demo lives

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/api/health")
 def read_root():
     return "Hello World!"
+
+class Link: 
+    display_name: str
+    url: str
+
+links: list[Link] = []
+
+link1 = Link()
+link1.display_name = "Youtube"
+link1.url = "https://youtube.com"
+
+link2 = Link()
+link2.display_name = "CoStar"
+link2.url = "https://costarastrology.com"
+
+links.append(link1)
+links.append(link2)
+
+@app.get("/api/links")
+def get_links():
+    return links
