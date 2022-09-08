f9288c863c839f7bbac0d654f521b49f849d72eb
Colin Brockman <cmbrock@unc.edu>
2022-09-08 18:25:39 -0400

    First workshop things, code is all API and backend

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from database import Base, engine, get_db
+from schemas import Link
+from models import LinkCreate
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 @app.get("/api/health")
 def read_root():
     return "Hello World!"
+
+links: list[Link] = []
+
+@app.post("/api/link")
+def create_link(link: LinkCreate, db = Depends(get_db)) -> Link:
+    db_link = Link(display_name = link.display_name, url = link.url)
+    db.add(db_link)
+    db.commit()
+    db.refresh(db._link)
+    return db_link
+
+@app.get("/api/links")
+def get_links(db = Depends(get_db)):
+    return db.query(Link).all()
---
+++ back-end/schemas.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, VARCHAR
+from database import Base
+
+class Link(Base):
+    __tablename__ = "links"
+
+    id = Column(Integer, primary_key=True, index=True)
+    display_name = Column(VARCHAR(256), index=True)
+    url = Column(VARCHAR(2083), index=True)
